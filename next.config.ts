468889763807f6927c7136d3e2ea06c048a865a1
Italo520 <italo520@gmail.com>
2026-02-21 10:51:59 -0300

    Correção nas dependencias, configs e icones do pwa

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
+  register: true,
 });
 
 const nextConfig: NextConfig = {
